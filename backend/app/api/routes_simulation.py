51cf1a287243ee3c4fb459cdf2b3826d15121a29
Fernando <fernandoesc180@gmail.com>
2025-09-29 13:42:01 -0600

    cambios de comentados

--- backend/app/api/routes_simulation.py
+++ backend/app/api/routes_simulation.py
@@ -8,6 +8,7 @@ router = APIRouter(prefix="/simulate", tags=["simulate"])
 
 #Plantilla basica 
 
+
 @router.post("/impact", response_model=SimResult)
 async def simulate_impact(body: SimInput):
     try:
@@ -18,26 +19,28 @@ async def simulate_impact(body: SimInput):
 
 #Con esto es para que funcione la simulacion de impacto
 
-#@router.post("/impact", response_model=GeoJson)
-#async def simulate_impact(payload: SimInput):
-#    import math
-#    lon, lat = payload.lon, payload.lat
-#    r_km = max(2.0, min(25.0, payload.diameter_m / 20.0))
-#    steps = 32
-#    coords = []
-#    for i in range(steps + 1):
-#        ang = 2 * math.pi * i / steps
-#        dlat = (r_km / 111.32) * math.cos(ang)
-#        dlon = (r_km / (111.32 * math.cos(math.radians(lat)))) * math.sin(ang)
-#        coords.append([lon + dlon, lat + dlat])
-#    return {
-#        "type": "FeatureCollection",
-#        "features": [
-#            {
-#                "type": "Feature",
-#                "geometry": {"type": "Polygon", "coordinates": [coords]},
-#                "properties": {"diameter_m": payload.diameter_m, "velocity_kms": payload.velocity_kms},
-#            },
-#            {"type": "Feature", "geometry": {"type": "Point", "coordinates": [lon, lat]}, "properties": {"label": "Impact"}},
-#        ],
-#    }
+'''
+@router.post("/impact", response_model=GeoJson)
+async def simulate_impact(payload: SimInput):
+    import math
+    lon, lat = payload.lon, payload.lat
+    r_km = max(2.0, min(25.0, payload.diameter_m / 20.0))
+    steps = 32
+    coords = []
+    for i in range(steps + 1):
+        ang = 2 * math.pi * i / steps
+        dlat = (r_km / 111.32) * math.cos(ang)
+        dlon = (r_km / (111.32 * math.cos(math.radians(lat)))) * math.sin(ang)
+        coords.append([lon + dlon, lat + dlat])
+    return {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {"type": "Polygon", "coordinates": [coords]},
+                "properties": {"diameter_m": payload.diameter_m, "velocity_kms": payload.velocity_kms},
+            },
+            {"type": "Feature", "geometry": {"type": "Point", "coordinates": [lon, lat]}, "properties": {"label": "Impact"}},
+        ],
+    }
+'''

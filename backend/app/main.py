a34db0fb737fa045b32676a1ec4eaeb4ff13c913
Fernando <fernandoesc180@gmail.com>
2025-09-29 14:17:43 -0600

    Ya no es necesario poner uvicorn

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,3 +17,12 @@ app.include_router(sim_router, prefix="/api/simulate", tags=["simulate"])
 @app.get("/api/health")
 def health():
     return {"ok": True}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",   # módulo:ruta_objeto
+        host="0.0.0.0",   # accesible desde red local
+        port=8000,        # puerto
+        reload=True       # autoreload en desarrollo
+    )
